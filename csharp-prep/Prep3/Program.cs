22a1818b475b07a8c97bf8128cd2e1fcf5e9cb52
bcushenberry <cushenberry@gmail.com>
2023-09-22 03:26:43 +0900

    Completed prep assignments 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int magNum = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int magNum = randomGenerator.Next(1, 101);
+
+        int guessNum = 0;
+
+        Console.WriteLine("Try guessing the magic number! ");
+        while (guessNum != magNum)
+        {
+            Console.Write("What is your guess? ");
+            guessNum = int.Parse(Console.ReadLine());
+
+            if (guessNum < magNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNum > magNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userInput = -1;
+        int total = 0;
+        int largest = 0;
+
+        Console.WriteLine("Enter a list of numbers and type 0 when finished.");
+
+        while (userInput != 0)
+        {
+            Console.Write("Enter number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+
+        }
+        
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            total += numbers[i];
+
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+        }
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine("The sum is: " + total);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: "+ largest);
     }
 }
