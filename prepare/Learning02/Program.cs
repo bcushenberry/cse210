8a37cf672ef9b40fcf46e263f2280933d5b32437
bcushenberry <cushenberry@gmail.com>
2023-09-29 05:04:07 +0900

    Completed the learning activity for W03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Self-employed";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2021;
+        job1._endYear = 2045;
+
+        Job job2 = new Job();
+        job2._company = "Keywords";
+        job2._jobTitle = "Senior Project Manager";
+        job2._startYear = 2012;
+        job2._endYear = 2021;
+
+        Resume resume = new Resume();
+        resume._name = "Brandon Cushenberry";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
+
     }
 }
+
+/*
+Class: Job
+Attributes:
+_company: string
+_jobTitle: string
+_startYear: int
+_endYear: int
+
+Behaviors:
+DisplayJobInfo(): void
+
+Class: Resume
+Attributes:
+_name: string
+_jobs: List<Job>
+
+Behaviors:
+DisplayResume(): void
+
+*/
