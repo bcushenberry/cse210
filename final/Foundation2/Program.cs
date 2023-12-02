00c259f2632e65e20b1c2b960fc446eee26af7e7
bcushenberry <cushenberry@gmail.com>
2023-12-02 20:07:43 +0900

    Clarified some variable names

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -10,24 +10,24 @@ class Program
         Customer c1 = new Customer("Bob Bobson", a1);
         Customer c2 = new Customer("Kate Kateson", a2);
 
-        List<Product> pl1 = new List<Product>();
-        pl1.AddRange(new List<Product>
+        List<Product> pList1 = new List<Product>();
+        pList1.AddRange(new List<Product>
         {
             new Product("Toothpaste", 55340, 2.50, 6),
             new Product("Apple", 22222, 0.49, 20),
             new Product("Sweater", 86454, 33.33, 1)
         });        
 
-        List<Product> pl2 = new List<Product>();
-        pl2.AddRange(new List<Product>
+        List<Product> pList2 = new List<Product>();
+        pList2.AddRange(new List<Product>
         {
             new Product("Rice - 20kg", 10001, 10.00, 2),
             new Product("Toothpaste", 55340, 2.50, 5),
             new Product("Blanket", 96811, 24.99, 1)
         });       
 
-        Order order1 = new Order(pl1, c1);
-        Order order2 = new Order(pl2, c2);
+        Order order1 = new Order(pList1, c1);
+        Order order2 = new Order(pList2, c2);
 
         Console.WriteLine("");
         Console.WriteLine("Order 1");
