3ce64bc1b767d0d85888a6d8d507b48a871cbc56
bcushenberry <cushenberry@gmail.com>
2023-12-06 01:43:30 +0900

    Deleted comment

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -11,12 +11,6 @@ public class Activity
         _activityName = activityName; 
     }
 
-// Example of DateTime in constructor:
-// Animal pet = new Animal("a12344", new DateTime(2013, 1, 23), "Fluffy");
-// var date1 = new DateTime(2008, 3, 1, 7, 0, 0);
-// Console.WriteLine(date1.ToString());
-// For en-US culture, displays 3/1/2008 7:00:00 AM
-
     public string GetSummary()
     {
         return $"{_date.ToShortDateString()} {_activityName} ({_minutes} min) - Distance {string.Format("{0:0.00}", GetDistance())} km, Speed {string.Format("{0:0.0}", GetSpeed())} kph, Pace: {string.Format("{0:0.0}", GetPace())} min per km";
